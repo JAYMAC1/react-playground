c44c16656f85b4367c6c3da7fcbd62aa503eebca
James McNeil <mcneil_james@hotmail.com>
2022-03-15 21:04:45 +0000

    initial commit

---
+++ src/pages/create/Create.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './create.scss'
+
+const Create = () => {
+  return (
+    <div className='container'>
+      <form className='form'>
+        <h2>Register</h2>
+      </form>
+    </div>
+  )
+}
+
+export default Create
