98f25be90c28a2ba8b30755a1fbac888cd21e3bf
James McNeil <mcneil_james@hotmail.com>
2022-03-16 22:59:32 +0000

    added BrowserRouter and Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CreateProject from './components/CreateProject'
+
 import Create from './pages/create/Create'
+
 function App() {
   return (
     <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Create />} />
+          <Route path='/signup' element={<Create />} />
+          <Route path='/add-project' element={<CreateProject />} />
+        </Routes>
+      </BrowserRouter>
       <Create />
     </div>
   )
