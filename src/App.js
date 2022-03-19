9dcf6671f6c4eadc41a3fcec3cf86898ca2566ba
James McNeil <mcneil_james@hotmail.com>
2022-03-19 17:14:30 +0000

    removed Create component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CreateProject from './components/CreateProject'
 
-import Create from './pages/create/Create'
-
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Create />} />
-          <Route path='/signup' element={<Create />} />
+          <Route path='/' element={<CreateProject />} />
+          {/* <Route path='/signup' element={<Create />} /> */}
           <Route path='/add-project' element={<CreateProject />} />
         </Routes>
       </BrowserRouter>
-      <Create />
     </div>
   )
 }
