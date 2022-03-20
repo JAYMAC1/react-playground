0c686dd9895aba52e1aeb3195c51f215defbcf2a
James McNeil <mcneil_james@hotmail.com>
2022-03-20 21:26:22 +0000

    initial creation of modal and styles

---
+++ src/components/Modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div className='modal-backdrop'>
+      <div className='modal'>
+        <h2>Add Milestone</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
